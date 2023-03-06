c314f42d02829cca4b929bbfafebdee311639ac4
Synchro <suren.zakaryan177@gmail.com>
2023-03-06 21:58:42 +0400

    Init Project

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index';
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app');
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import "@/assets/css/tailwind.css"
+
+// createApp(App).mount('#app')
