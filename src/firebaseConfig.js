bf643197da1b2c9fb6c68a41087a4bdeab310f47
Synchro <suren.zakaryan177@gmail.com>
2023-03-08 21:22:02 +0400

    Change FiraBase credentials

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: "AIzaSyCnCg8miAUi6IHqvK68wysyS37AUGX1my0",
-    authDomain: "practice8-vue3-firebaseui-app.firebaseapp.com",
-    projectId: "practice8-vue3-firebaseui-app",
-    storageBucket: "practice8-vue3-firebaseui-app.appspot.com",
-    messagingSenderId: "623503090265",
-    appId: "1:623503090265:web:92183f62bc14489ce50748",
-    measurementId: "G-6D4L7KKB3W"
-  };
+  apiKey: 'AIzaSyBbCEgocpnYzZcr12jSqtLWf6N6PYy6vCg',
+  authDomain: 'vue3-todo-2ed43.firebaseapp.com',
+  projectId: 'vue3-todo-2ed43',
+  storageBucket: 'vue3-todo-2ed43.appspot.com',
+  messagingSenderId: '59572246311',
+  appId: '1:59572246311:web:02a375b48850714b22fb02',
+  measurementId: 'G-FZTZL4DHQS',
+};
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,33 +1,33 @@
 <template>
-  <div class="relative h-screen">
+  <div class="relative h-screen bg-slate-50">
     <div
       class="absolute max-w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
     >
-    <div id="firebaseui-auth-container"></div>
-    <div id="loader">Loading...</div>
-    <br>
-    <div v-if="user.isSignedIn">
-      <button @click="handleSignOut">Sign Out</button>
+      <div id="firebaseui-auth-container"></div>
+      <div id="loader">Loading...</div>
+      <br />
+      <div v-if="user.isSignedIn">
+        <button @click="handleSignOut">Sign Out</button>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 // Firebase
-import firebaseConfig from '@/firebaseConfig.js';
+import firebaseConfig from "@/firebaseConfig.js";
 
 // Pinia
-import { useUserStore } from '@/stores/user'
+import { useUserStore } from "@/stores/user";
 
 // v9 compat packages are API compatible with v8 code
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
 
 firebase.initializeApp(firebaseConfig);
-import * as firebaseui from 'firebaseui'
-import 'firebaseui/dist/firebaseui.css'
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
 import { getAuth, signOut } from "firebase/auth";
 
 const auth = getAuth();
@@ -38,60 +38,62 @@ export default {
     const user = useUserStore();
 
     const uiConfig = {
-      signInFlow: 'popup',
-      signinSuccessUrl: 'http://localhost:8080/',
+      signInFlow: "popup",
+      signinSuccessUrl: "http://localhost:8080/",
 
       signInOptions: [
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        firebase.auth.GithubAuthProvider.PROVIDER_ID
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
       ],
 
       callbacks: {
         signInSuccessWithAuthResult: function (authResult) {
           user.value = authResult.user.displayName;
-          user.setUsername(authResult.user.displayName)
+          user.setUsername(authResult.user.displayName);
           user.isSignedIn = true;
-          router.push('/');
- 
+          router.push("/");
+
           // so it doesn't refresh the page
           return false;
         },
-        uiShown: function() {
+        uiShown: function () {
           // The widget is rendered.
           // Hide the loader.
-          document.getElementById('loader').style.display = 'none';
-        }
-      }
-    }
+          document.getElementById("loader").style.display = "none";
+        },
+      },
+    };
 
     // Initialize the FirebaseUI Widget using Firebase.
     try {
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      ui.start('#firebaseui-auth-container', uiConfig);
+      ui.start("#firebaseui-auth-container", uiConfig);
     } catch {
       window.location.reload();
     }
 
     const handleSignOut = () => {
-      signOut(auth).then(() => {
-       // Sign-out successful.
-       user.value = null;
-       user.isSignedIn = false;
-       console.log('Signed out');
-       ui.start('#firebaseui-auth-container', uiConfig);
-      }).catch((error) => {
-        // An error happened.
-        console.log(error);
-      });
-    }
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          user.value = null;
+          user.isSignedIn = false;
+          console.log("Signed out");
+          ui.start("#firebaseui-auth-container", uiConfig);
+        })
+        .catch((error) => {
+          // An error happened.
+          console.log(error);
+        });
+    };
 
     return {
       user,
       handleSignOut,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
